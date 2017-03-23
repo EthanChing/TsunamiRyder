742d5bfcbcffc97028e8cb31a7931aaeee197456
Arthur Brandao <Arthuranbu@gmail.com>
2017-03-22 21:28:07 -0700

    Time Blink Mechanic
    Added the Rewind time blink
    Organized folders

---
+++ TsunamiRyder/Assets/Scripts/TimeRewind.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class TimeRewind : MonoBehaviour
+{
+
+   
+    private InputDevice controller;
+    public int playerNum;
+    private Vector3 lastPoint;
+    private float currentTime;
+    public float maxTime;
+
+    void Update()
+
+    {
+
+        controller = InputManager.Devices[playerNum];
+        currentTime += Time.deltaTime;
+        if (currentTime > maxTime)
+        {
+            currentTime = 0;
+            lastPoint = this.transform.position;
+        }
+        if (controller.Action2.WasPressed)
+        {
+            transform.position = lastPoint;
+        }
+
+    }       
+}
